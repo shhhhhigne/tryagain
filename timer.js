a767c954e881dae3faa716eafa7bf3c78b061102
Signe Henderson <signe_henderson@brown.edu>
2016-08-24 17:57:32 -0700

    updated node

--- timer.js
+++ timer.js
@@ -3,11 +3,21 @@ var app = express();
 const os = require('os');
 
 var serverStartTime = os.uptime();
+var lastAPICall = -1;
 
 app.get('/', function(req, res) {
 	serverRunTime = os.uptime() - serverStartTime;
+	if(lastAPICall == -1) {
+		apiRunTime = 0;
+	}
+	else {
+		apiRunTime = os.uptime() - lastAPICall;
+	}
+	console.log('api run = ' + apiRunTime);
+	lastAPICall = os.uptime();
 	console.log('HI!');
-	console.log(serverRunTime);
+	console.log('server run = ' + serverRunTime);
+
 });
 
 app.get('/check', function(req,res) {
