dcecda73e8656fbe44eaaf0be3cebaf190888dbd
Signe Henderson <signe_henderson@brown.edu>
2016-10-22 12:56:07 -0700

    I made the counter a more self contained object and fixed a bug where every time the counter page was loaded a new timer was added (still a new timer is added, but when you leave the counter page the old timer is deleted)

--- counter/counter/ViewController.swift
+++ counter/counter/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var timer = Timer()
-    var resetTimer = Timer()
+    //var timer = Timer()
+    //var resetTimer = Timer()
     var getTime = Timer()
     
-    let counter = CounterClass()
+    var counter = CounterClass()
 
     @IBOutlet weak var counterLabel: UILabel!
     
@@ -22,10 +22,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //timer = Timer.scheduledTimer(timeInterval: 1, target: counter, selector: #selector(counter.incrementCount), userInfo: nil, repeats: true)
+        //resetTimer = Timer.scheduledTimer(timeInterval: 100, target: counter, selector: #selector(counter.resetCount), userInfo: nil, repeats: true)
         
-        timer = Timer.scheduledTimer(timeInterval: 1, target: counter, selector: #selector(counter.incrementCount), userInfo: nil, repeats: true)
-        
-        resetTimer = Timer.scheduledTimer(timeInterval: 100, target: counter, selector: #selector(counter.resetCount), userInfo: nil, repeats: true)
+        counter.setIncrement(interval: 1)
+        counter.setReset(interval: 100)
         
         getTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.display), userInfo: nil, repeats: true)
             }
@@ -41,12 +42,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillDisappear(_ animated: Bool){
+        super.viewWillDisappear(animated)
+        print("left page")
+        getTime.invalidate()
+        counter.invalidateTimers()
+    }
 
 
 }
 
 class CounterClass {
     var count = 0
+    var intTimer = Timer()
+    var resetTimer = Timer()
+
+    @objc func invalidateTimers() {
+        intTimer.invalidate()
+        resetTimer.invalidate()
+    }
+
+    @objc func setIncrement(interval: Double) {
+        intTimer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(incrementCount), userInfo: nil, repeats: true)
+    }
+    
+    @objc func setReset(interval: Double) {
+        resetTimer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(resetCount), userInfo: nil, repeats: true)
+    }
+    
     @objc func getCount() -> Int {
         return count
     }
